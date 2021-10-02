dcac4073558c3166006a15c02ffaa362aa5faf5d
Danil Rybkin <teddychanel03@gmail.com>
2021-10-02 21:44:27 +0300

    dinamic creation  employers-list-item

--- src/components/employers-list/employers-list.js
+++ src/components/employers-list/employers-list.js
@@ -2,12 +2,24 @@ import EmployersListItem from '../employers-list-item/employers-list-item'
 
 import './employers-list.css';
 
-const EmployersList = () => {
+const EmployersList = ({data}) => {
+
+    const elements = data.map(item => {
+
+        const {id, name, salary, increase} = item;
+
+        return (
+            <EmployersListItem
+                key={id}  
+                name={name} 
+                salary={salary} 
+                increase={increase}/>
+        )
+    });
+
     return (
         <ul className="app-list list-group">
-            <EmployersListItem/>
-            <EmployersListItem/>
-            <EmployersListItem/>
+            {elements}
         </ul>
     )
 }
